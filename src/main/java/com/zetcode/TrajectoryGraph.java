d43fbc5ec10004486fe3132c6b3616beee973453
supermengman <andrewm17us@gmail.com>
2022-09-02 10:44:10 -0700

    new implementation of trajectoryMath

---
+++ src/main/java/com/zetcode/TrajectoryGraph.java
@@ -0,0 +1,33 @@
+package com.zetcode;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.block.BlockBorder;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.chart.title.TextTitle;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+import com.math.TrajectoryMath;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+
+public class TrajectoryGraph extends JFrame {
+
+    
+
+    public static void main(String[] args) {
+        TrajectoryMath test = new TrajectoryMath(10, 30, 40);
+        System.out.println(test.getValue(4));
+        
+    }
+}
