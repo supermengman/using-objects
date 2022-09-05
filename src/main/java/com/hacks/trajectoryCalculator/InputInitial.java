6c8eb07e88bb00e75b2c44786af2f65cd36e6ae5
supermengman <andrewm17us@gmail.com>
2022-09-04 23:37:39 -0700

    error handling + more clear text

--- src/main/java/com/hacks/trajectoryCalculator/InputInitial.java
+++ src/main/java/com/hacks/trajectoryCalculator/InputInitial.java
@@ -4,31 +4,44 @@ import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 public class InputInitial {
-    public static double initialVelocity;
-    public static double initialDegrees;
-    public static double initialHeight;
+    public static Double initialVelocity;
+    public static Double initialDegrees;
+    public static Double initialHeight;
 
     public void spawnInputs() {
-        JTextField inputVelocity = new JTextField();
-        JTextField inputDegrees = new JTextField();
-        JTextField inputHeight = new JTextField();
+        while (initialVelocity == null || initialDegrees == null || initialHeight == null) {
+            JTextField inputVelocity = new JTextField();
+            JTextField inputDegrees = new JTextField();
+            JTextField inputHeight = new JTextField();
 
-        Object[] inputs = {
-            "Initial Velocity:", inputVelocity,
-            "Initial Degrees:", inputDegrees,
-            "Initial Height:", inputHeight
-        };
+            Object[] inputs = {
+                "Initial Velocity (m/s):", inputVelocity,
+                "Initial Degrees:", inputDegrees,
+                "Initial Height (m):", inputHeight
+            };
 
-        JOptionPane.showConfirmDialog(null, inputs, "Input the initial values for your object:", JOptionPane.OK_CANCEL_OPTION);
+            JOptionPane.showConfirmDialog(null, inputs, "Input the initial values for your object (numbers only):", JOptionPane.OK_CANCEL_OPTION);
 
-        initialVelocity = parseInput(inputVelocity);
-        initialDegrees = parseInput(inputDegrees);
-        initialHeight = parseInput(inputHeight);
+            initialVelocity = parseInput(inputVelocity);
+            initialDegrees = parseInput(inputDegrees);
+            initialHeight = parseInput(inputHeight);
+        }
+        
     }
 
-    public double parseInput(JTextField inputValue) {
+    public Double parseInput(JTextField inputValue) {
         String placeholder = inputValue.getText();
-        double initialValue = Double.parseDouble(placeholder);
-        return initialValue;
+        try {
+            double initialValue = Double.parseDouble(placeholder);
+            if (initialValue < 0.0) {
+                JOptionPane.showMessageDialog(null, "Inputs must be greater than 0", "Invalid Input", JOptionPane.WARNING_MESSAGE);
+                return null;
+            } else {
+                return initialValue;
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "There was an invalid input for " + placeholder + ", please try again. " + e, "Unwanted Input", JOptionPane.WARNING_MESSAGE);
+            return null;
+        }
     }
 }
