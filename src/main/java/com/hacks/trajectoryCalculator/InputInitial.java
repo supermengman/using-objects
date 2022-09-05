b47fcf0212da5a133144b22a0db3061dc917a5bb
supermengman <andrewm17us@gmail.com>
2022-09-05 00:04:52 -0700

    comments on graph display + inputs

--- src/main/java/com/hacks/trajectoryCalculator/InputInitial.java
+++ src/main/java/com/hacks/trajectoryCalculator/InputInitial.java
@@ -1,27 +1,33 @@
 package com.hacks.trajectoryCalculator;
 
-import javax.swing.JOptionPane;
-import javax.swing.JTextField;
+import javax.swing.JOptionPane; // library to display options
+import javax.swing.JTextField; // library to create a text field to render on GUI
 
 public class InputInitial {
+    // instance variables to be used
     public static Double initialVelocity;
     public static Double initialDegrees;
     public static Double initialHeight;
 
+    // create the GUI element that users input into
     public void spawnInputs() {
+        // while the values have not changed yet, keep going (for error handling)
         while (initialVelocity == null || initialDegrees == null || initialHeight == null) {
+            // text field initialization
             JTextField inputVelocity = new JTextField();
             JTextField inputDegrees = new JTextField();
             JTextField inputHeight = new JTextField();
 
+            // organizing the input text to display + the text field in object
             Object[] inputs = {
                 "Initial Velocity (m/s):", inputVelocity,
                 "Initial Degrees:", inputDegrees,
                 "Initial Height (m):", inputHeight
             };
 
-            JOptionPane.showConfirmDialog(null, inputs, "Input the initial values for your object (numbers only):", JOptionPane.OK_CANCEL_OPTION);
+            JOptionPane.showConfirmDialog(null, inputs, "Input the initial values for your object (numbers only):", JOptionPane.OK_CANCEL_OPTION); // creates the option menu with the 3 inputs
 
+            // take the input, assign it to the public variables
             initialVelocity = parseInput(inputVelocity);
             initialDegrees = parseInput(inputDegrees);
             initialHeight = parseInput(inputHeight);
@@ -29,17 +35,23 @@ public class InputInitial {
         
     }
 
+    // change JTextField into Double, also error handling
     public Double parseInput(JTextField inputValue) {
-        String placeholder = inputValue.getText();
+        String placeholder = inputValue.getText(); // get the string out of the input
+        
+        // error handling + edge cases
         try {
             double initialValue = Double.parseDouble(placeholder);
+
+            // if negative, cannot be valid so throw error --> reinput values bc still null
             if (initialValue < 0.0) {
                 JOptionPane.showMessageDialog(null, "Inputs must be greater than 0", "Invalid Input", JOptionPane.WARNING_MESSAGE);
                 return null;
-            } else {
+            } else { // if everything ok, return the value
                 return initialValue;
             }
-        } catch (Exception e) {
+            
+        } catch (Exception e) { // if cannot be cased as a double, throw error --> reinput values
             JOptionPane.showMessageDialog(null, "There was an invalid input for " + placeholder + ", please try again. " + e, "Unwanted Input", JOptionPane.WARNING_MESSAGE);
             return null;
         }
--- src/main/java/com/hacks/trajectoryCalculator/TrajectoryGraph.java
+++ src/main/java/com/hacks/trajectoryCalculator/TrajectoryGraph.java
@@ -1,4 +1,6 @@
-package com.hacks.trajectoryCalculator;
+package com.hacks.trajectoryCalculator; // maven build
+
+// import the graphs
 import org.jfree.data.function.Function2D;
 import org.jfree.data.general.*;
 import org.jfree.chart.ChartFactory;
@@ -7,42 +9,52 @@ import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.xy.XYDataset;
 
+// import the other files
 import com.hacks.trajectoryCalculator.*;
 
+// import swing and awt
 import javax.swing.JFrame;
 import java.awt.Dimension;
 
 public class TrajectoryGraph extends JFrame {
-    public double currentHeight;
+    // Constructor of the graph that will be displayed
     public TrajectoryGraph() {
-        InputInitial newInputs = new InputInitial();
-        newInputs.spawnInputs();
-       drawGraph(InputInitial.initialVelocity, InputInitial.initialDegrees, InputInitial.initialHeight);
+        InputInitial newInputs = new InputInitial(); // start input object from separate file
+        newInputs.spawnInputs(); // start the process of collecting user input
+       drawGraph(InputInitial.initialVelocity, InputInitial.initialDegrees, InputInitial.initialHeight); // call method to draw the graph with the user input taken
     }
 
+    // graph drawer
     public void drawGraph(double velocity, double degrees, double height) {
-        Function2D test = new TrajectoryMath(velocity, degrees, height);
-        TrajectoryMath testGetter = new TrajectoryMath(velocity, degrees, height);
-        XYDataset dataset = DatasetUtils.sampleFunction2D(test, 0.0, testGetter.getRoot(), 50, "Function");
-        final JFreeChart chart = ChartFactory.createXYLineChart("Trajectory Equation", "X Position (meters)", "Y Position (meters)", dataset, PlotOrientation.VERTICAL, true, true, false);
+        Function2D test = new TrajectoryMath(velocity, degrees, height); // TrajectoryMath implements function2d, so use those values create new function2d object
+        TrajectoryMath testGetter = new TrajectoryMath(velocity, degrees, height); // creates TrajectoryMath object from the other file to call custom methods
+        XYDataset dataset = DatasetUtils.sampleFunction2D(test, 0.0, testGetter.getRoot(), 50, "Function"); // generates the dataset of xy values with the function
+        final JFreeChart chart = ChartFactory.createXYLineChart("Trajectory Equation", "X Position (meters)", "Y Position (meters)", dataset, PlotOrientation.VERTICAL, true, true, false); // creates the actual graph with attributes
+        
+        // initializing the display
         ChartPanel cp = new ChartPanel(chart) {
 
             @Override
             public Dimension getPreferredSize() {
-                return new Dimension(320, 240);
+                return new Dimension(500, 500); // set initial dimension
             }
         };
+
+        // allow mouse wheel scrolling
         cp.setMouseWheelEnabled(true);
         add(cp);
+
+        // Finalize the building of the graph
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         pack();
     }
     public static void main(String[] args) {
     
-
+        // runs the creation of the graph with a queue in a different thread and posts the gui after events are processed
         java.awt.EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
+                // initialize new TrajectoryGraph object, setvisible to display the graph
                 new TrajectoryGraph().setVisible(true);
             }
         });
